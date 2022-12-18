2f21a8a996598d68d3d0a890d3c1350ca0141bd1
nahhuaraujo <nahhuaraujo@gmail.com>
2022-12-17 21:42:18 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as S from './App.styled';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <S.App>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
-    </div>
+    </S.App>
   );
-}
+};
 
 export default App;
---
+++ src/styles/global-styles.styled.ts
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components';
+
+export const lightestGreenColorValue = '#a0facc';
+export const lightGreenColorValue = '#26de81';
+export const darkGreenColorValue = '#20bf6b';
+export const lightGreyColorValue = '#d1d8e0';
+export const darkGreyColorValue = '#a5b1c2';
