2cd6a6d0469282c2f4535822113c12ba20f39b47
nahhuaraujo <nahhuaraujo@gmail.com>
2023-02-11 22:11:48 -0300

    Refactor move BrowserRouter and Redux Provider to App index

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import * as S from './App.styled';
 import { Footer, Header, Spinner } from './components';
 import { AuthGuard } from './guards';
 import { About, CreateReport, Home, Login, NotFound, Register, Report } from './pages';
-import store from './redux/store';
 import { PrivateRoutes, PublicRoutes } from './routes';
 
 const App = () => {
   return (
     <S.App>
-      <Provider store={store}>
-        <Router>
-          <Header />
-          <Spinner />
-          <Routes>
-            <Route path={PublicRoutes.HOME} element={<Home />} />
-            <Route path={PublicRoutes.LOGIN} element={<Login />} />
-            <Route path={PublicRoutes.REGISTER} element={<Register />} />
-            <Route path={PublicRoutes.ABOUT_US} element={<About />} />
-            <Route element={<AuthGuard />}>
-              <Route path={PrivateRoutes.CREATE_REPORT} element={<CreateReport />} />
-              <Route path={`${PrivateRoutes.REPORT}/:id`} element={<Report />} />
-            </Route>
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </Provider>
+      <Header />
+      <Spinner />
+      <Routes>
+        <Route path={PublicRoutes.HOME} element={<Home />} />
+        <Route path={PublicRoutes.LOGIN} element={<Login />} />
+        <Route path={PublicRoutes.REGISTER} element={<Register />} />
+        <Route path={PublicRoutes.ABOUT_US} element={<About />} />
+        <Route element={<AuthGuard />}>
+          <Route path={PrivateRoutes.CREATE_REPORT} element={<CreateReport />} />
+          <Route path={`${PrivateRoutes.REPORT}/:id`} element={<Report />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
     </S.App>
   );
 };
